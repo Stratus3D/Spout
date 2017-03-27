68b1535325ef992fde91caae2b4525a57f925e21
Trevor Brown <admin@stratus3d.com>
2017-03-26 21:57:44 -0400

    Misc improvements.

--- test/spout_test.exs
+++ test/spout_test.exs
@@ -56,15 +56,15 @@ defmodule SpoutTest do
     [passing_1, passing_2, passing_3|skipped_suite] = tests
 
     # Passing tests
-    passing_test(passing_1, 1, "passing test", :ok)
+    passing_test(passing_1, 1, "passing test")
     failing_test(passing_2, 2, "failing test")
-    passing_test(passing_3, 3, "description", :ok)
+    passing_test(passing_3, 3, "description")
 
     # Then the usage suite
     [todo, failing, diagnostic|footer_lines] = skipped_suite
     todo_test(todo, 4, "todo test")
     skipped_test(failing, 5, "skip test")
-    passing_test(diagnostic, 6, "diagnostic test", :ok)
+    passing_test(diagnostic, 6, "diagnostic test")
 
     # Last line should be the test plan (skipped suite is excluded the report)
     [test_plan, _] = footer_lines
@@ -72,35 +72,22 @@ defmodule SpoutTest do
   end
 
   # Private functions
-  defp passing_test(line, number, test_name, return) do
+  defp passing_test(line, number, test_name) do
     number_bin = Integer.to_string(number)
-    return_bin = IO.iodata_to_binary(:io_lib.format("~w", [return]))
     expected = <<"ok ", number_bin :: binary, " test ", test_name :: binary>>
     ^expected = line
   end
 
-  defp failing_test(line, number, test) do
-    failing_test(line, number, test, nil)
-  end
-  defp failing_test(line, number, test_name, reason) do
+  defp failing_test(line, number, test_name) do
     number_bin = Integer.to_string(number)
     expected = <<"not ok ", number_bin :: binary, " test ", test_name :: binary>>
     ^expected = line
   end
 
   defp skipped_test(line, number, test_name) do
-    skipped_test(line, number, test_name, nil)
-  end
-  defp skipped_test(line, number, test_name, reason) do
     number_bin = Integer.to_string(number)
     expected = <<"ok ", number_bin :: binary, " test ", test_name :: binary, " # SKIP">>
-    case reason do
-      nil ->
-        ^expected = line
-          _ ->
-            real_expected = <<expected :: binary, " ", reason :: binary>>
-            ^real_expected = line
-        end
+    ^expected = line
   end
 
   defp todo_test(line, number, test_name) do
