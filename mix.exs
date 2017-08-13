52e8ae66729276a87f430bfd1eaa89cd744a5144
Trevor Brown <admin@stratus3d.com>
2017-08-13 15:33:00 -0400

    Bump version to 0.1.0.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Spout.Mixfile do
 
   def project do
     [app: :spout,
-     version: "0.0.1",
+     version: "0.1.0",
      elixir: "~> 1.0",
      description: description,
      package: package,
